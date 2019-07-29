32520cb1c91d7e59784cc0f2b8d054bf93cf36a1
yourtechsis <aretee64@gmail.com>
2019-07-29 04:39:42 -0600

    testing files change

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const port =  process.env.PORT || 3000;
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
 app.use(express.static('css'));
+app.listen(port, function () {
+    console.log(`Example app listening on port !` + port);
+   });
+
 
 
 app.get('/index.html', function (req, res) {
@@ -20,9 +26,5 @@ app.post('/submit-student-data', function (req, res) {
     console.log(req.body.firstName)
 });
 
-var server = app.listen(8081, function () {
-    var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", port)
-})
+
 
