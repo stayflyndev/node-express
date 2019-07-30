77bda97a033e44818013bb24d313fd90888e8809
yourtechsis <aretee64@gmail.com>
2019-07-29 19:16:20 -0600

    updates

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const port =  process.env.PORT || 3000;
+const router = express.Router();
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-
 app.use(express.static('css'));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+const port =  process.env.PORT || 3000;
 app.listen(port, function () {
     console.log(`Example app listening on port !` + port);
    });
 
+   
+ app.get('/', function (req, res) {
+    res.sendFile( __dirname + "/" + "index.html" )    // res.sendFile( __dirname + "/" + "index.html" )
+});
+app.get('/contact', function (req, res) {
+    res.sendFile( __dirname + "/" + "contact.html" )
+  })
 
 
-app.get('/', function (req, res) {
-    res.sendFile( __dirname + "/" + "index.html" )
-})
+
 
 app.post('/submit-student-data', function (req, res) {
     var name = req.body.firstName + ' ' + req.body.lastName;
